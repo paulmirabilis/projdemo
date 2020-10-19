c48d3cb0f0398f33ffce2c18f8ae70eadce136b1
edureka-adebayo <paulmirabilis@gmail.com>
2020-10-19 14:01:22 +0530

    commit done for 2 files

---
+++ adebayo.java
@@ -0,0 +1,8 @@
+class adebayo{
+
+      public static void main(String[] args){
+          System.out.println("This is created by adebayo as demo prog");
+          System.out.println("Good Morming Everyone ");
+      }
+
+}
